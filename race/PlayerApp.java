597a65a91fb7cc80f5f13cee865e3e1a6286742c
nk9009 <nk9009@naver.com>
2020-06-10 17:15:00 +0900

    17:03

--- race/PlayerApp.java
+++ race/PlayerApp.java
@@ -23,17 +23,19 @@ public class PlayerApp {
 		
 		Player[] ranking = new Player[3]; // ranking 기록에 의한 순서
 		if(players[0].getRecord() < players[1].getRecord()) {
-			System.out.println(players[0].getName()
-					+"이 "
-					+players[1].getName()+"보다 빠르다");
-		} else {
-			System.out.println(players[1].getName() 
-					+"이 "
-					+players[0].getName()+"보다 빠르다");
+			if(players[0].getRecord() < players[2].getRecord()) {
+				ranking[0] = players[0];
+			} else {
+				ranking[0] = players[2];
+			}	
+		}else {
+			if(players[1].getRecord()<players[2].getRecord()) {
+				ranking[0] = players[1];
+			}else {
+				ranking[0] = players[2];
+			}
 		}
+		System.out.println("1등은 " + ranking[0].getName());
 	}
 }
-/**
- 
- */
 
